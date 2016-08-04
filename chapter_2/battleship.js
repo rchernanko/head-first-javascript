b506a2d7390d14f3fd56b1b0e0e4ab3f1a4ff178
Richard Chernanko <rchernanko@hotmail.co.uk>
2016-08-04 12:10:04 +0100

    finished the inital battleship game javascript

--- chapter_2/battleship.js
+++ chapter_2/battleship.js
@@ -16,7 +16,21 @@ while (isSunk == false) {
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1;
+
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits = hits + 1;
+            alert("HIT!");
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        } else {
+            alert("MISS!");
+        }
     }
-
-
 }
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " +
+    "which means your shooting accuracy was " + (hits / guesses);
+alert(stats);
